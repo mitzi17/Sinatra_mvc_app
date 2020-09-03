27ceb70e1e2c4827084d6839b533df439569394d
Mitzi17 <m_estrada17@live.com>
2020-09-03 16:16:28 -0700

    add private methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,24 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     set :sessions, true
     set :session_secret, ENV["SESSION_SECRET"]
+    set :method_override, true
   end
 
   get "/" do
     erb :homepage
+  end
+
+  private 
+
+  def current_user 
+    # this method returns the currently logged in user if there is one, and nil if there isn't
+    # allows to add conditional logic in the view to display different options to logged in users
+    User.find_by_id(session[:id])
+  end
+
+  def logged_in?
+    # this method returns true or false
+    !!current_user
   end
 
 end

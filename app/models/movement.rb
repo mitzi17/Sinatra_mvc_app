68eb346cad56b0762ed43c4f0284abd8c9533c3c
Mitzi17 <m_estrada17@live.com>
2020-09-11 19:36:39 -0700

    add validations to Movement class

--- app/models/movement.rb
+++ app/models/movement.rb
@@ -1,5 +1,10 @@
 class Movement < ActiveRecord::Base
     belongs_to :user
     validates :destination, presence: true
-    
+    validates :date, presence: true
+    validates :season, presence: true
+    validates :category, presence: true
+    validates :box_number, presence: true, uniqueness: true
+    validates :units_quantity, presence: true
+
 end

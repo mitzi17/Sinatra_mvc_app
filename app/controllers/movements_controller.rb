6b8588b7f90a0be1a0fcf8fcc17c405a760f0437
Mitzi17 <m_estrada17@live.com>
2020-09-14 20:00:29 -0700

    add flash messages

--- app/controllers/movements_controller.rb
+++ app/controllers/movements_controller.rb
@@ -18,6 +18,7 @@ class MovementsController < ApplicationController
     redirect_if_not_logged_in
     @movement = current_user.movements.build(date: params[:movement][:date],season: params[:movement][:season],category: params[:movement][:category],destination: params[:movement][:destination],box_number: params[:movement][:box_number],units_quantity: params[:movement][:units_quantity])
     if @movement.save
+      flash[:success] = "Movement successfully created."
     redirect "/movements"
     else
     erb :"/movements/new.html"
@@ -26,23 +27,23 @@ class MovementsController < ApplicationController
 
   # GET: /movements/5 -> show
   get "/movements/:id" do
-    set_movement
+    get_movement
     erb :"/movements/show.html"  
   end
 
   # GET: /movements/5/edit -> edit
   get "/movements/:id/edit" do
-    set_movement
+    get_movement
     redirect_if_not_authorized
     erb :"/movements/edit.html" 
   end
 
   # PATCH: /movements/5 -> update
   patch "/movements/:id" do
-    set_movement
+    get_movement
     redirect_if_not_authorized
     if @movement.update(date: params[:movement][:date],season: params[:movement][:season],category: params[:movement][:category],destination: params[:movement][:destination],box_number: params[:movement][:box_number],units_quantity: params[:movement][:units_quantity])
-      flash[:success] = "Movement successfully updated"
+      flash[:success] = "Movement successfully updated."
       redirect "/movements/#{@movement.id}" 
     else 
       erb :"/movements/edit.html"
@@ -51,18 +52,19 @@ class MovementsController < ApplicationController
 
   # DELETE: /movements/5/delete -> delete
   delete "/movements/:id" do
-    set_movement
+    get_movement
     redirect_if_not_authorized
     @movement.destroy
+    flash[:success] = "Movement successfully deleted."
     redirect "/movements"
   end
 
   private
 
-  def set_movement 
+  def get_movement 
     @movement = Movement.find_by_id(params[:id])
     if @movement.nil?
-      flash[:error] = "Couldn't find a Movement with id: #{params[:id]}"
+      flash[:error] = "Couldn't find a movement with id: #{params[:id]}"
       redirect "/movements"
     end
   end
@@ -70,7 +72,7 @@ class MovementsController < ApplicationController
   def redirect_if_not_authorized
     redirect_if_not_logged_in
     if !authorize_movement(@movement) && !manager_user(current_user)
-      flash[:error] = "You don't have permission to do that action"
+      flash[:error] = "Permission denied. You can only edit or delete your own movements."
       redirect "/movements"
     end
   end
@@ -78,12 +80,5 @@ class MovementsController < ApplicationController
   def authorize_movement(movement)
     current_user  == movement.user
   end
-
-  
-
-  
-
- 
-  
 
 end
